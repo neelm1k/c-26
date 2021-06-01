f67d87e1cfa5b1e405f4f698d2e5e123a143fd19
neel1k <neel.mirpuri1@gmail.com>
2021-06-01 16:51:34 -0400

    git

--- sketch.js
+++ sketch.js
@@ -16,12 +16,12 @@ function setup(){
     world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
+    ground = new Ground(600,height,1200,25);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    box1 = new Box(700,320,78,70);
+    box2 = new Box(920,325,78,70);
+    pig1 = new Pig(812, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -30,11 +30,11 @@ function setup(){
 
     log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
+    box5 = new Box(820,160,72,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(1800,75);
 
 }
 
